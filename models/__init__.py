9ad89d4539eab751b43b4c19170f0db34960ece2
davidmigabo9-boop <davidmigabo9@gmail.com>
2026-07-22 11:12:16 +0200

    Add models/__init__.py

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from models.user import Utilisateur
+from models.joueur import Joueur, Contribution
+from models.budget import Budget
+from models.equipement import Equipement
+
+__all__ = ["Utilisateur", "Joueur", "Contribution", "Budget", "Equipement"]
