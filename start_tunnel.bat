8ec4c93aa2ba2c663ef06bc0f90e58feebeca5ef
davidmigabo9-boop <davidmigabo9@gmail.com>
2026-07-22 11:14:58 +0200

    Delete start_tunnel.bat

--- start_tunnel.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Demarrage de Flask...
-cd /d "C:\Users\LENOVO\Documents\New OpenCode Project\fc_vainqueur_web"
-start /b python app.py > nul 2>&1
-timeout /t 4 /nobreak > nul
-echo Demarrage du tunnel SSH...
-ssh -o StrictHostKeyChecking=no -N -R 80:localhost:5000 nokey@localhost.run > url.txt 2>&1
