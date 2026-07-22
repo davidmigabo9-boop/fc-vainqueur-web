a6cee78fa84a6846b4e02b3ee9aadd18daa6b703
davidmigabo9-boop <davidmigabo9@gmail.com>
2026-07-22 13:27:45 +0200

    Update routes/main.py

--- routes/main.py
+++ routes/main.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 from models.joueur import Joueur, Contribution
 from models.budget import Budget
 from models.equipement import Equipement
 from models.audit import AuditLog
 from datetime import datetime
+from routes import role_required
 
 main_bp = Blueprint("main", __name__)
 
@@ -12,11 +13,14 @@ main_bp = Blueprint("main", __name__)
 @main_bp.route("/")
 @login_required
 def index():
-    return redirect(url_for("main.dashboard"))
+    if current_user.has_perm("dashboard_voir"):
+        return redirect(url_for("main.dashboard"))
+    return redirect(url_for("joueurs.joueurs_list"))
 
 
 @main_bp.route("/dashboard")
 @login_required
+@role_required("dashboard_voir")
 def dashboard():
     now = datetime.now()
     mois = now.month
